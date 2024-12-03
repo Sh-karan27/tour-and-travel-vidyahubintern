7cdfa405337900997154ffbfe914fc3a87086906
Sh-karan27 <shuklakaran107@gmail.com>
2024-12-03 23:55:55 +0530

    content changes added in Home section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { NavLink } from 'react-router';
 const Navbar = () => {
   return (
     <div className='w-full p-4 flex items-center justify-evenly'>
-      <div className='flex items-center justify-center gap-1 text-heading-default'>
+      <div className='flex items-center justify-center gap-1 text-red-500'>
         <img src={logo} alt='logo' className='w-10 h-10' />
         <span className='text-xl font-bold'>Travel</span>
       </div>
@@ -16,20 +16,16 @@ const Navbar = () => {
           <NavLink
             to={curr.link}
             key={i}
-            className='relative font-semibold text-text-normal hover:text-heading-default group'>
+            className='relative font-semibold text-text-normal hover:text-red-500 group'>
             {curr.name}
-            <span className='absolute left-0 bottom-0 h-[2px] w-0 bg-heading-default transition-all duration-500 group-hover:w-full'></span>
+            <span className='absolute left-0 bottom-0 h-[2px] w-0 bg-red-500 transition-all duration-500 group-hover:w-full'></span>
           </NavLink>
         ))}
       </div>
 
       <div className='flex items-center justify-center gap-2'>
-        <button className='border rounded-md p-2 text-heading-default'>
-          IND|ENG
-        </button>
-        <button className='border rounded-md p-2 text-heading-default'>
-          INR
-        </button>
+        <button className='border rounded-md p-2 text-red-500'>IND|ENG</button>
+        <button className='border rounded-md p-2 text-red-500'>INR</button>
       </div>
     </div>
   );
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className='w-full h-screen'>
       {/* Hero Section */}
       <div
-        className='h-1/2 w-full bg-cover bg-center flex items-center justify-center'
+        className='h-full w-full bg-cover bg-center flex flex-col items-center justify-evenly'
         style={{ backgroundImage: `url(${Hero})` }}>
         <div className='text-center text-white'>
           <h1 className='text-4xl font-bold'>Travelling Around the World</h1>
@@ -17,46 +17,48 @@ const Home = () => {
             Continue Reading
           </button>
         </div>
-      </div>
 
-      {/* Input Section */}
-      <div className=' mt-10 w-full bg-white flex items-center justify-center'>
-        <div className='w-3/4 bg-gray-100 shadow-md rounded-lg p-6'>
-          <form className='flex justify-between gap-4'>
-            <div className='flex flex-col w-1/4'>
-              <label className='mb-2 text-gray-700'>Search Destination*</label>
-              <input
-                type='text'
-                placeholder='Delhi'
-                className='p-2 border rounded focus:outline-none focus:ring focus:ring-blue-300'
-              />
-            </div>
-            <div className='flex flex-col w-1/4'>
-              <label className='mb-2 text-gray-700'>Pax Number*</label>
-              <input
-                type='number'
-                placeholder='2'
-                className='p-2 border rounded focus:outline-none focus:ring focus:ring-blue-300'
-              />
-            </div>
-            <div className='flex flex-col w-1/4'>
-              <label className='mb-2 text-gray-700'>Checkin Date*</label>
-              <input
-                type='date'
-                className='p-2 border rounded focus:outline-none focus:ring focus:ring-blue-300'
-              />
-            </div>
-            <div className='flex flex-col w-1/4'>
-              <label className='mb-2 text-gray-700'>Checkout Date*</label>
-              <input
-                type='date'
-                className='p-2 border rounded focus:outline-none focus:ring focus:ring-blue-300'
-              />
-            </div>
-            <button className='self-end px-6 py-2 bg-red-500 text-white font-semibold rounded hover:bg-red-600'>
-              Inquire Now
-            </button>
-          </form>
+        {/* Input Section */}
+        <div className=' mt-10  bg-white flex items-center justify-center'>
+          <div className=' bg-gray-100 shadow-md rounded-lg p-6'>
+            <form className='flex justify-between gap-4'>
+              <div className='flex flex-col w-1/4'>
+                <label className='mb-2 text-gray-700'>
+                  Search Destination*
+                </label>
+                <input
+                  type='text'
+                  placeholder='Delhi'
+                  className='p-2 border rounded focus:outline-none focus:ring focus:ring-blue-300'
+                />
+              </div>
+              <div className='flex flex-col w-1/4'>
+                <label className='mb-2 text-gray-700'>Pax Number*</label>
+                <input
+                  type='number'
+                  placeholder='2'
+                  className='p-2 border rounded focus:outline-none focus:ring focus:ring-blue-300'
+                />
+              </div>
+              <div className='flex flex-col w-1/4'>
+                <label className='mb-2 text-gray-700'>Checkin Date*</label>
+                <input
+                  type='date'
+                  className='p-2 border rounded focus:outline-none focus:ring focus:ring-blue-300'
+                />
+              </div>
+              <div className='flex flex-col w-1/4'>
+                <label className='mb-2 text-gray-700'>Checkout Date*</label>
+                <input
+                  type='date'
+                  className='p-2 border rounded focus:outline-none focus:ring focus:ring-blue-300'
+                />
+              </div>
+              <button className='self-end px-6 py-2 bg-red-500 text-white font-semibold rounded hover:bg-red-600'>
+                Inquire Now
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
