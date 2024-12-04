5335a7b67001054a4952edf3e63bb615efbeade8
ratnaraj7 <ratnarajdaimary7@gmail.com>
2024-12-04 13:53:27 +0530

    add dropdown to nav

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,60 +1,61 @@
-import React from 'react';
-import Hero from '..//assets/images/HeroSection.jpeg';
+import React from "react";
+import Hero from "..//assets/images/HeroSection.jpeg";
 
 const Home = () => {
   return (
-    <div className='w-full h-screen'>
+    <div className="w-full h-screen">
       {/* Hero Section */}
       <div
-        className='h-full w-full bg-cover bg-center flex flex-col items-center justify-evenly'
-        style={{ backgroundImage: `url(${Hero})` }}>
-        <div className='text-center text-white'>
-          <h1 className='text-4xl font-bold'>Travelling Around the World</h1>
-          <p className='mt-4 text-lg'>
+        className="h-full w-full bg-cover bg-center flex flex-col items-center justify-evenly"
+        style={{ backgroundImage: `url(${Hero})` }}
+      >
+        <div className="text-center text-white">
+          <h1 className="text-4xl font-bold">Travelling Around the World</h1>
+          <p className="mt-4 text-lg">
             Taciti quasi, sagittis excepteur hymenaeos, id temporibus hic.
           </p>
-          <button className='mt-6 px-6 py-2 bg-red-500 text-white font-semibold rounded hover:bg-red-600'>
+          <button className="mt-6 px-6 py-2 bg-red-500 text-white font-semibold rounded hover:bg-red-600">
             Continue Reading
           </button>
         </div>
 
         {/* Input Section */}
-        <div className=' mt-10  bg-white flex items-center justify-center'>
-          <div className=' bg-gray-100 shadow-md rounded-lg p-6'>
-            <form className='flex justify-between gap-4'>
-              <div className='flex flex-col w-1/4'>
-                <label className='mb-2 text-gray-700'>
+        <div className=" mt-10  bg-white flex items-center justify-center">
+          <div className=" bg-gray-100 shadow-md rounded-lg p-6">
+            <form className="flex justify-between gap-4">
+              <div className="flex flex-col w-1/4">
+                <label className="mb-2 text-gray-700">
                   Search Destination*
                 </label>
                 <input
-                  type='text'
-                  placeholder='Delhi'
-                  className='p-2 border rounded focus:outline-none focus:ring focus:ring-blue-300'
+                  type="text"
+                  placeholder="Delhi"
+                  className="p-2 border rounded focus:outline-none focus:ring focus:ring-blue-300"
                 />
               </div>
-              <div className='flex flex-col w-1/4'>
-                <label className='mb-2 text-gray-700'>Pax Number*</label>
+              <div className="flex flex-col w-1/4">
+                <label className="mb-2 text-gray-700">Pax Number*</label>
                 <input
-                  type='number'
-                  placeholder='2'
-                  className='p-2 border rounded focus:outline-none focus:ring focus:ring-blue-300'
+                  type="number"
+                  placeholder="2"
+                  className="p-2 border rounded focus:outline-none focus:ring focus:ring-blue-300"
                 />
               </div>
-              <div className='flex flex-col w-1/4'>
-                <label className='mb-2 text-gray-700'>Checkin Date*</label>
+              <div className="flex flex-col w-1/4">
+                <label className="mb-2 text-gray-700">Checkin Date*</label>
                 <input
-                  type='date'
-                  className='p-2 border rounded focus:outline-none focus:ring focus:ring-blue-300'
+                  type="date"
+                  className="p-2 border rounded focus:outline-none focus:ring focus:ring-blue-300"
                 />
               </div>
-              <div className='flex flex-col w-1/4'>
-                <label className='mb-2 text-gray-700'>Checkout Date*</label>
+              <div className="flex flex-col w-1/4">
+                <label className="mb-2 text-gray-700">Checkout Date*</label>
                 <input
-                  type='date'
-                  className='p-2 border rounded focus:outline-none focus:ring focus:ring-blue-300'
+                  type="date"
+                  className="p-2 border rounded focus:outline-none focus:ring focus:ring-blue-300"
                 />
               </div>
-              <button className='self-end px-6 py-2 bg-red-500 text-white font-semibold rounded hover:bg-red-600'>
+              <button className="self-end px-6 py-2 bg-red-500 text-white font-semibold rounded hover:bg-red-600">
                 Inquire Now
               </button>
             </form>
